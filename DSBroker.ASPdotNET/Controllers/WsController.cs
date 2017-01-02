2a347bc6f5a08310415779316fb93864f838e0ad
Logan Gorence <loganjohngorence@gmail.com>
2017-01-02 11:48:14 -0800

    Simple echo WebSocket handling.

---
+++ DSBroker.ASPdotNET/Controllers/WsController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace DSBroker.ASPdotNET.Controllers
+{
+    [Route("ws")]
+    public class WsController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
--- DSBroker.ASPdotNET/Startup.cs
+++ DSBroker.ASPdotNET/Startup.cs
@@ -3,6 +3,11 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
+using System;
+using System.Diagnostics;
+using System.Net.WebSockets;
+using System.Text;
+using System.Threading;
 
 namespace DSBroker.ASPdotNET
 {
@@ -32,6 +37,39 @@ namespace DSBroker.ASPdotNET
         {
             loggerFactory.AddConsole(Configuration.GetSection("Logging"));
             loggerFactory.AddDebug();
+
+            app.UseWebSockets();
+            app.Use(async (http, next) =>
+            {
+                if (http.Request.Path == "/ws" && http.WebSockets.IsWebSocketRequest)
+                {
+                    var webSocket = await http.WebSockets.AcceptWebSocketAsync();
+                    if (webSocket != null && webSocket.State == WebSocketState.Open)
+                    {
+                        while (webSocket.State == WebSocketState.Open)
+                        {
+                            var token = CancellationToken.None;
+                            var buffer = new ArraySegment<byte>(new byte[4096]);
+
+                            var received = await webSocket.ReceiveAsync(buffer, token);
+
+                            switch (received.MessageType)
+                            {
+                                case WebSocketMessageType.Text:
+                                    var request = Encoding.UTF8.GetString(buffer.Array,
+                                                                          buffer.Offset,
+                                                                          buffer.Count);
+                                    await webSocket.SendAsync(buffer, WebSocketMessageType.Text, true, CancellationToken.None);
+                                    break;
+                            }
+                        }
+                    }
+                }
+                else
+                {
+                    await next();
+                }
+            });
 
             app.UseMvc();
         }
